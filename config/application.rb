e217dcef66ab0440c81f352fc837b211f0efd2ec
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-07 23:52:28 +0900

    アプリケーションの国際化、Railsのタイムゾーンを設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module TaskManagement
   class Application < Rails::Application
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
