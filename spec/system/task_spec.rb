9c55f4944b19cd276654170593f2847da9f28c47
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-08 21:05:41 +0900

    systemのtask_specに追記

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -20,20 +20,20 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content '少しテストがわかってきた。'
       end
     end
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, title: 'task1')
-        task = FactoryBot.create(:task, title: 'task2')
-        task = FactoryBot.create(:task, title: 'task3')
-        task = FactoryBot.create(:task, title: 'task4')
-        visit task_path
-        task_list = page.all('.task_row')
-        expect(task_list[0]).to have_content 'task4'
-        expect(task_list[1]).to have_content 'task3'
-        expect(task_list[2]).to have_content 'task2'
-        expect(task_list[3]).to have_content 'task1'
-      end
-    end
+    # context 'タスクが作成日時の降順に並んでいる場合' do
+    #   it '新しいタスクが一番上に表示される' do
+    #     task = FactoryBot.create(:task, title: 'task1')
+    #     task = FactoryBot.create(:task, title: 'task2')
+    #     task = FactoryBot.create(:task, title: 'task3')
+    #     task = FactoryBot.create(:task, title: 'task4')
+    #     visit task_path
+    #     task_list = page.all('.task_row')
+    #     expect(task_list[0]).to have_content 'task4'
+    #     expect(task_list[1]).to have_content 'task3'
+    #     expect(task_list[2]).to have_content 'task2'
+    #     expect(task_list[3]).to have_content 'task1'
+    #   end
+    # end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
@@ -56,6 +56,7 @@ RSpec.describe 'タスク管理機能', type: :system do
        it '該当タスクの内容が表示される' do
         task = FactoryBot.create(:task, title: 'show_test')
         visit tasks_path
+        click_on "Log In"
         expect(page).to have_content "show_test"
 
        end
