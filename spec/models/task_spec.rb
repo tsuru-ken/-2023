bcd7e379d27c30df33092663f99f61957c79b2ea
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-15 21:15:32 +0900

    Model Speの通過を確認

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -25,14 +25,12 @@ RSpec.describe 'タスクモデル機能', type: :model do
     end
   end
 
-  describe '検索機能' do
-
-  end
 
   describe '検索機能' do
     # 必要に応じて、テストデータの内容を変更して構わない
     let!(:task) { FactoryBot.create(:task, title: 'task') }
     let!(:second_task) { FactoryBot.create(:second_task, title: "sample") }
+
     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
         # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
@@ -44,10 +42,18 @@ RSpec.describe 'タスクモデル機能', type: :model do
     context 'scopeメソッドでステータス検索をした場合' do
       it "ステータスに完全一致するタスクが絞り込まれる" do
         # ここに内容を記載する
+        expect(Task.search_status('not_started')).to include(task)
+        expect(Task.search_status('in_progress')).to include(second_task)
+        expect(Task.search_status('not_started').count).to eq 1
+
       end
     end
     context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        expect(Task.search_title('task').search_status('not_started')).to include(task)
+        expect(Task.search_title('sample').search_status('in_progress')).to include(second_task)
+        expect(Task.search_title('sample').search_status('in_progress').count).to eq 1
+
         # ここに内容を記載する
       end
     end
