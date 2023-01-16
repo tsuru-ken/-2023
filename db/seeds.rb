e15169348b16c85507fa312dd30d28560126ba7c
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-17 00:30:13 +0900

    user table作成とuser.rbへバリデーション作成とseed.rbの記載

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "test@test.com",
+  name: "テスト太郎",
+  password: "123456",
+  admin: true)
+  User.create!(
+  email: "task@task.com",
+  name: "タスク太郎",
+  password: "123456",
+  admin: false)
