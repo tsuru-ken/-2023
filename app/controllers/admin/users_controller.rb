79f20efc62416c03b84602cebc82038a5cd54955
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-20 21:25:14 +0900

    adminのルーティング作成＆index画面作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,7 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only:  [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "tasks#index"
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  namespace :admin do
+    resources :users
+  end
   resources :tasks do
     collection do
       post :confirm
