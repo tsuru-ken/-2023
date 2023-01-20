3da905e658c1e4b59d254fa7a9040384c3088f20
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-21 00:23:23 +0900

    adminのidをとるための質問

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,42 @@ class Admin::UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tasks = current_user.tasks
+  end
+
+  def edit
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+    unless @user
+      flash[:alert] = "User not found."
+      redirect_to admin_users_path and return
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation,:admin)
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation,:admin)
   end
 
   # def correct_user
