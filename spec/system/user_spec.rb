b62698bd782a09595b2c6452ae187db0edc9c07b
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-21 22:24:06 +0900

    specのfactoriesのuser.rbの記載

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー管理機能', type: :system do
+  describe '登録機能' do
+    context 'ユーザーを登録した場合' do
+      it 'タスク一覧画面に遷移する' do
+        
+      end
+    end
+  end
+end
