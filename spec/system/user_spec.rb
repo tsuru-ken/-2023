9f95fde8c389d82703bd713388e1ab2da18c2060
takeshi tsuruda <lazywayofcat@gmail.com>
2023-01-21 23:52:51 +0900

    セッション機能のテスト完了

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザー管理機能', type: :system do
+  let!(:user){FactoryBot.create(:user)}
+  let!(:admin_user){FactoryBot.create(:admin_user)}
+
   describe '登録機能' do
     context 'ユーザーを登録した場合' do
       it 'タスク一覧画面に遷移する' do
@@ -21,5 +24,50 @@ RSpec.describe 'ユーザー管理機能', type: :system do
       end
     end
   end
-  
+  describe 'セッション機能' do
+    context 'ログインした場合' do
+      it 'プロフィールに移動できる' do
+        visit new_session_path
+        fill_in 'session[email]', with: user.email
+        fill_in 'session[password]', with: user.password
+        click_button "Log in"
+        visit user_path(user)
+        expect(current_path).to eq user_path(user)
+      end
+    end
+
+    context 'ログインした場合' do
+      it '自分の詳細ページに移動できる' do
+        visit new_session_path
+        fill_in 'session_email', with: user.email
+        fill_in 'session_password', with: user.password
+        click_button "Log in"
+        visit user_path(user)
+        expect(current_path).to eq user_path(user)
+      end
+    end
+    context 'ログアウトした場合' do
+      it 'ログイン画面に戻る' do
+        visit new_session_path
+        fill_in 'session[email]', with: user.email
+        fill_in 'session[password]', with: user.password
+        click_button "Log in"
+        visit user_path(user)
+        click_link "Logout"
+        expect(current_path).to eq new_session_path
+      end
+    end
+  end
+  describe '管理画面機能' do
+    context '管理ユーザーがログインした状態で管理画面に遷移した場合' do
+      it '管理画面に移動' do
+        visit new_session_path
+        fill_in 'session[email]', with: user.email
+        fill_in 'session[password]', with: user.password
+        click_button "Log in"
+        visit user_path(user)
+        expect(current_path).to eq user_path(user)
+      end
+    end
+  end
 end
